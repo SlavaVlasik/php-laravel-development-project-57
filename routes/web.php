453acb84c8b6735f8b342ae540139814bdb7a13c
Slava <s_lav1k@mail.ru>
2025-02-02 22:21:50 +0300

    add rollbar config

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test-rollbar', function () {
+    \Log::debug('Test debug message');
+    return 'Test message sent to Rollbar!';
+});
